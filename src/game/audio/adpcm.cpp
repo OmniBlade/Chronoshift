4ec2cfff3f3a51b3f4547aaddbf8eb32a7c11074
OmniBlade <nadia_xy@hotmail.com>
2020-04-11 23:02:19 +0100

    Fixes missing file header for adpcm.cpp.

--- src/game/audio/adpcm.cpp
+++ src/game/audio/adpcm.cpp
@@ -1,4 +1,18 @@
-
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief  Decoders for the IMA ADPCM and Westwood ADPCM formats.
+ *
+ * @copyright Chronoshift is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
 #include "adpcm.h"
 #include "endiantype.h"
 #include <algorithm>
