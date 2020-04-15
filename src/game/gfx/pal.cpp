149f957558c802460964fe4fa60aa94e6b8b28c0
OmniBlade <nadia_xy@hotmail.com>
2020-04-15 22:48:32 +0100

    [GFX] Implements Increase_Palette_Luminance.
    Function used in VQA palette code.

--- src/game/gfx/pal.cpp
+++ src/game/gfx/pal.cpp
@@ -14,16 +14,31 @@
  */
 #include "pal.h"
 #include "initvideo.h"
+#include <algorithm>
 #include <cstring>
 
 using std::memcpy;
 
+enum
+{
+    PALETTE_SIZE = 768,
+};
+
 #ifndef GAME_DLL
-uint8_t g_CurrentPalette[768];
+uint8_t g_CurrentPalette[PALETTE_SIZE];
 #endif
 
 void Set_Palette(void *pal)
 {
-    memcpy(g_CurrentPalette, pal, 768);
+    memcpy(g_CurrentPalette, pal, PALETTE_SIZE);
     Set_Video_Palette(pal);
+}
+
+void Increase_Palette_Luminance(uint8_t *pal, int r, int g, int b, int max)
+{
+    for (int i = 0; i < PALETTE_SIZE; i += 3) {
+        pal[i] = std::min(max, pal[i] + r * pal[i] / 100);
+        pal[i + 1] = std::min(max, pal[i + 1] + g * pal[i + 1] / 100);
+        pal[i + 2] = std::min(max, pal[i + 2] + b * pal[i + 2] / 100);
+    }
 }
